ee4cc969873437eb05fbf90fdfd7b72f4c919f5f
root <programmer.nour@gmail.com>
2018-10-29 14:02:41 +0000

    Added the solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text:"This should mirror the text you typed into the input field."
+  }
+  
+constructor (){
+  super();
+  
+}
+
+textChanged = evnt=>
+{
+	this.setState({text : evnt.target.value});
+};
+  
   render() {
     return (
       <div className="App">
@@ -11,9 +25,9 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input type="text" onChange={this.textChanged} placeholder="Say Something" />
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.text}</p>
         </div>
       </div>
     );
